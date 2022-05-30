9c4ac3c22cedff4da549478c12bca2af177a06c6
Andi <andismt17@gmail.com>
2022-05-30 11:47:40 -0500

    Adicion de Account(Controller, Service y Repository)

---
+++ src/main/java/com/group2/com/group_proyect/account/model/entity/Account.java
@@ -0,0 +1,28 @@
+package com.group2.com.group_proyect.account.model.entity;
+
+import com.group2.com.group_proyect.Customer.model.entity.Customer;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account")
+@Data
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "number", length = 8, nullable = false)
+    private String number;
+
+    @Column(name = "balance", nullable = false)
+    private Double balance;
+
+    /*@ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;*/
+
+}
