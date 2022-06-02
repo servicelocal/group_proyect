c69ea184b9d5c6bd6b5c2e625b564be2fcebfb63
Randy <randy.aragon@ucsp.edu.pe>
2022-06-02 10:50:56 -0500

    añadido esquema de ejm de 'registros' de la clase

--- src/main/java/com/group2/com/group_proyect/creditcard/model/entity/Transacts_CreditCard.java
+++ src/main/java/com/group2/com/group_proyect/creditcard/model/entity/Transacts_CreditCard.java
@@ -3,11 +3,12 @@ package com.group2.com.group_proyect.creditcard.model.entity;
 public class Transacts_CreditCard {
 
 	/*
-	id_crediCard //5 , 5, 5
-	idCliBusiness//3, 3, 3
-	tipo_opracion(cargo o pago) //1, 2 , 2
-	monto // 20, 10, 10
-	fecha //hoy, currdate, currdate
+	id_creditCard		//5,  5,  5,  2,  2
+	id_Customer			//3,  3,  3,  1,  4
+	flg_CustomerType	//0,  0,  0,  1,  0	//(true=PersonalC, false:BussinessC )
+	flg_transactType	//1,  2,  2,  1,  1	//(true=charge, false=pay)
+	amount 				//20, 10, 10, 30, 15
+	dateHour			//_, _, _, _, _
 	
 	*/
 }
